28ca436b92771805fe1b99cc35aed1b052e47a36
Irrator <vancat220@gmail.com>
2023-10-15 00:03:33 +0300

    backup point

--- AeroturApp/MainPage.xaml.cs
+++ AeroturApp/MainPage.xaml.cs
@@ -1,13 +1,16 @@
-﻿namespace AeroturApp
+﻿
+namespace AeroturApp
 {
     public partial class MainPage : ContentPage
     {
+        List<string> VayWariants = new List<string>();
+        int selectedIndex = 0;
+
 
         public MainPage()
         {
             InitializeComponent();
         }
-
     }
 
 }
