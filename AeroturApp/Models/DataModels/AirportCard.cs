cff4acb7d4f92c0fd468028f101c61f730b197bf
Irrator <vancat220@gmail.com>
2023-10-17 23:11:53 +0300

    Start making web api client

---
+++ AeroturApp/Models/DataModels/AirportCard.cs
@@ -0,0 +1,15 @@
+﻿namespace AeroturApp.Models.DataModels
+{
+    class AirportCard
+    {
+        public Dictionary<string, string> NameTranslation { get; set; }
+        public string CityCode { get; set; }
+        public string CountryCode { get; set; }
+        public string TimeZone {  get; set; }
+        public string IATACode { get; set; }
+        public string IATAType { get; set; }
+        public string Name { get; set; }
+        public Dictionary<string, int> Coordinates { get; set; }
+        public bool Flightable { get; set; } = true;
+    }
+}
---
+++ AeroturApp/Services/WebAPIClients.cs
@@ -0,0 +1,31 @@
+﻿using AeroturApp.Models.DataModels;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AeroturApp.Services
+{
+    class WebAPIClients
+    {
+        //http://pics.avs.io/высота/ширина/ИАТА код.png для логотипов (для иос @2x)
+        static HttpClient client = new HttpClient();
+
+        static async Task<AirportCard> GetAirportAsync(string path)
+        {
+            AirportCard product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                JsonDocument JSONresponse = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+                product = new AirportCard();
+
+                
+            }
+            return product;
+        }
+
+    }
+}
